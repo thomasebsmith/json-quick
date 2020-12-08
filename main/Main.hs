af651adf688ef5d0cb2ae9b068aa83be037c236a
Thomas Smith <thsm@umich.edu>
2020-12-07 23:54:35 -0500

    Add inline documentation to Main.hs

--- main/Main.hs
+++ main/Main.hs
@@ -4,6 +4,8 @@ import System.Environment
 import System.Exit
 import CLI
 
+-- | 'main' is the entry point for the json-quick command line interface.
+-- For more details about argument parsing, see src/CLI.hs.
 main :: IO ()
 main = do
   parsedArgs <- parseArgs <$> getArgs
@@ -17,12 +19,19 @@ main = do
     Left errorMsg -> do
       showBadUsage errorMsg
 
+-- | The 'showBadUsage' function shows an error (msg) and then shows general
+-- usage and help information. It then causes the program to exit with a
+-- non-zero exit status.
 showBadUsage :: String -> IO ()
 showBadUsage msg = do
   putErrLn msg
   showUsage
   exitFailure
 
+-- | The 'showError' function shows an error (msg) and then causes the program
+-- to exit with a non-zero exit status. This is used instead of 'showBadUsage'
+-- when the general syntax of an invocation was correct, but it produced an
+-- error.
 showError :: String -> IO ()
 showError msg = do
   putErrLn msg
